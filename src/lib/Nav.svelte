d72c47baa4a03b94af972e9bbad98e71a80a7524
Suphakrit Boonlar <153996957+aisuphakrit@users.noreply.github.com>
2023-12-30 19:13:25 +0700

    Updated Href

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -18,38 +18,38 @@
 				</svg>
 			</button>
 			<ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-				<li><a href="#">หน้าแรก</a></li>
+				<li><a href="/">หน้าแรก</a></li>
 				<li>
-					<a href="#">เติมเงิน</a>
+					<a href="/topup">เติมเงิน</a>
 					<ul class="p-2">
-						<li><a href="#">ซองอังเปา</a></li>
-						<li><a href="#">คิวอาร์โค้ด</a></li>
+						<li><a href="/angpao">ซองอังเปา</a></li>
+						<li><a href="/qrcode">คิวอาร์โค้ด</a></li>
 					</ul>
 				</li>
-				<li><a href="#">สินค้าและบริการ</a></li>
-				<li><a href="#" class="btn btn-sm mb-1 mt-2">สมัครสมาชิก</a></li>
-				<li><a href="#" class="btn btn-sm mt-1">ลงชื่อเข้าใช้</a></li>
+				<li><a href="/products">สินค้าทิ้งหมด</a></li>
+				<li><a href="/register" class="btn btn-sm mb-1 mt-2">สมัครสมาชิก</a></li>
+				<li><a href="/login" class="btn btn-sm mt-1">ลงชื่อเข้าใช้</a></li>
 			</ul>
 		</div>
 		<button class="btn btn-ghost text-xl">Template</button>
 	</div>
 	<div class="navbar-center hidden lg:flex">
 		<ul class="menu menu-horizontal px-1">
-			<li><a href="#">หน้าแรก</a></li>
+			<li><a href="/">หน้าแรก</a></li>
 			<li>
 				<details>
 					<summary>เติมเงิน</summary>
 					<ul class="p-2 w-36">
-						<li><a href="#">ซองอังเปา</a></li>
-						<li><a href="#">คิวอาร์โค้ด</a></li>
+						<li><a href="/angpao">ซองอังเปา</a></li>
+						<li><a href="/qrcode">คิวอาร์โค้ด</a></li>
 					</ul>
 				</details>
 			</li>
-			<li><a href="#">สินค้าและบริการ</a></li>
+			<li><a href="/products">สินค้าทิ้งหมด</a></li>
 		</ul>
 	</div>
 	<div class="navbar-end hidden lg:flex">
-		<a href="#" class="btn mr-1">สมัครสมาชิก</a>
-		<a href="#" class="btn ml-1">ลงชื่อเข้าใช้</a>
+		<a href="/register" class="btn mr-1">สมัครสมาชิก</a>
+		<a href="/login" class="btn ml-1">ลงชื่อเข้าใช้</a>
 	</div>
 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,7 +6,7 @@
 				<input type="text" placeholder="ชื่อผู้ใช้" class="input input-bordered max-w-xs" />
                 <input type="password" placeholder="รหัสผ่าน" class="input input-bordered max-w-xs" />
                 <button class="btn">ลงชื่อเข้าใช้</button>
-                <a href="#" class="text-sm">ลืมรหัสผ่าน?</a>
+                <a href="/recovery" class="text-sm">ลืมรหัสผ่าน?</a>
 			</form>
 		</div>
 	</div>
