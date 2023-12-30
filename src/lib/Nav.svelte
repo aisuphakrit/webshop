25dd867e48041e12496707a291b834987ee6d18f
Suphakrit Boonlar <153996957+aisuphakrit@users.noreply.github.com>
2023-12-30 17:01:49 +0700

    Created Navbar

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,55 @@
+<div class="navbar bg-base-100">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<button type="button" class="btn btn-ghost lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/>
+				</svg>
+			</button>
+			<ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+				<li><a href="#">หน้าแรก</a></li>
+				<li>
+					<a href="#">เติมเงิน</a>
+					<ul class="p-2">
+						<li><a href="#">ซองอังเปา</a></li>
+						<li><a href="#">คิวอาร์โค้ด</a></li>
+					</ul>
+				</li>
+				<li><a href="#">สินค้าและบริการ</a></li>
+				<li><a href="#" class="btn btn-sm mb-1">สมัครสมาชิก</a></li>
+				<li><a href="#" class="btn btn-sm mt-1">ลงชื่อเข้าใช้</a></li>
+			</ul>
+		</div>
+		<button class="btn btn-ghost text-xl">Template</button>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="#">หน้าแรก</a></li>
+			<li>
+				<details>
+					<summary>เติมเงิน</summary>
+					<ul class="p-2 w-36">
+						<li><a href="#">ซองอังเปา</a></li>
+						<li><a href="#">คิวอาร์โค้ด</a></li>
+					</ul>
+				</details>
+			</li>
+			<li><a href="#">สินค้าและบริการ</a></li>
+		</ul>
+	</div>
+    <div class="navbar-end hidden lg:flex">
+        <a href="#" class="btn mr-1">สมัครสมาชิก</a>
+        <a href="#" class="btn ml-1">ลงชื่อเข้าใช้</a>
+    </div>
+</div>
