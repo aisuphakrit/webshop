676abb6fd1ac6850004b087f93119c9f7b7e3cf7
Suphakrit Boonlar <153996957+aisuphakrit@users.noreply.github.com>
2023-12-30 19:02:54 +0700

    Created Login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<div>
+	<div class="flex justify-center items-center text-center">
+		<div class="grid gap-5">
+			<h1 class="text-xl font-bold">ลงชื่อเข้าใช้</h1>
+			<form action="" class="grid gap-2">
+				<input type="text" placeholder="ชื่อผู้ใช้" class="input input-bordered max-w-xs" />
+                <input type="password" placeholder="รหัสผ่าน" class="input input-bordered max-w-xs" />
+                <button class="btn">ลงชื่อเข้าใช้</button>
+                <a href="#" class="text-sm">ลืมรหัสผ่าน?</a>
+			</form>
+		</div>
+	</div>
+</div>
