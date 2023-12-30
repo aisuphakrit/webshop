dcefdb19de51f67b28540aeb28769571935ed0f8
Suphakrit Boonlar <153996957+aisuphakrit@users.noreply.github.com>
2023-12-30 19:04:41 +0700

    Created Register

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,13 @@
+<div>
+	<div class="flex justify-center items-center text-center">
+		<div class="grid gap-5">
+			<h1 class="text-xl font-bold">สมัครสมาชิก</h1>
+			<form action="" class="grid gap-2">
+				<input type="text" placeholder="ชื่อผู้ใช้" class="input input-bordered max-w-xs" />
+                <input type="password" placeholder="รหัสผ่าน" class="input input-bordered max-w-xs" />
+				<input type="password" placeholder="ยืนยันรหัสผ่าน" class="input input-bordered max-w-xs" />
+                <button class="btn">สมัครสมาชิก</button>
+			</form>
+		</div>
+	</div>
+</div>
